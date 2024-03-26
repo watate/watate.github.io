e2a5fdb81416a5b18d92ca9a6c86727bbc507da7
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-03-26 12:23:37 +0800

    buttons now hide/show content

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-aboutButton = document.getElementById("about-button");
-readingsButton = document.getElementById("readings-button");
-aboutSection = document.getElementById("about");
-readingsSection = document.getElementById("readings");
+const aboutButton = document.getElementById("about-button");
+const readingsButton = document.getElementById("readings-button");
+const aboutSection = document.getElementById("about");
+const readingsSection = document.getElementById("readings");
+const photoSection = document.getElementById("photo");
 
 function hideContent(content) {
     content.classList.add("hide");
 }
 
-function showContent() {
+function showContent(content) {
     content.classList.remove("hide");
 }
+
+function onAboutButtonClick() {
+    showContent(aboutSection);
+    showContent(photoSection);
+    hideContent(readingsSection);
+}
+
+function onReadingsButtonClick() {
+    showContent(readingsSection);
+    hideContent(aboutSection);
+    hideContent(photoSection);
+}
+
+aboutButton.addEventListener("click", onAboutButtonClick);
+readingsButton.addEventListener("click", onReadingsButtonClick);
