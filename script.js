0d421e34999e4e34c2427e1a5e3e9bdc5d182f44
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-03-26 14:52:49 +0800

    Added dragonRPG and chamberlain projects

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const aboutButton = document.getElementById("about-button");
 const readingsButton = document.getElementById("readings-button");
+const projectsButton = document.getElementById("projects-button");
 const aboutSection = document.getElementById("about");
 const readingsSection = document.getElementById("readings");
 const photoSection = document.getElementById("photo");
+const projectsSection = document.getElementById("projects");
 
 function hideContent(content) {
     content.classList.add("hide");
@@ -16,6 +18,7 @@ function onAboutButtonClick() {
     showContent(aboutSection);
     showContent(photoSection);
     hideContent(readingsSection);
+    hideContent(projectsSection);
     window.location.hash = 'photo';
 }
 
@@ -23,8 +26,18 @@ function onReadingsButtonClick() {
     showContent(readingsSection);
     hideContent(aboutSection);
     hideContent(photoSection);
+    hideContent(projectsSection);
     window.location.hash = 'readings';
+}
+
+function onProjectsButtonClick() {
+    showContent(projectsSection);
+    hideContent(aboutSection);
+    hideContent(photoSection);
+    hideContent(readingsSection);
+    window.location.hash = 'projects';
 }
 
 aboutButton.addEventListener("click", onAboutButtonClick);
 readingsButton.addEventListener("click", onReadingsButtonClick);
+projectsButton.addEventListener("click", onProjectsButtonClick);
