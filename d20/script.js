85f6a4da35144c520ebb11495b8cca0b73d46003
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2024-03-30 16:12:51 +0800

    Added localStorage to keep track of how many decisions helped

--- d20/script.js
+++ d20/script.js
@@ -3,11 +3,14 @@ const helpCount = document.getElementById("help-count");
 const d20btn = document.getElementById("d20-btn");
 const coinbtn = document.getElementById("coin-btn");
 
-let count = 0;
+let count = parseInt(localStorage.getItem("count")) || 0;
+
+helpCount.textContent = `helped you make ${count} decisions`;
 
 const onD20roll = () => {
     const roll = Math.floor(Math.random() * 20) + 1;
     count++;
+    localStorage.setItem("count", count.toString());
     result.textContent = `Result: ${roll}`;
     helpCount.textContent = `helped you make ${count} decisions`;
 }
@@ -15,6 +18,7 @@ const onD20roll = () => {
 const onCoinFlip = () => {
     const flip = Math.random() >= 0.5;
     count++;
+    localStorage.setItem("count", count.toString());
     result.textContent = `Result: ${flip ? "Heads" : "Tails"}`;
     helpCount.textContent = `helped you make ${count} decisions`;
 }
